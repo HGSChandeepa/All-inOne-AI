3b3b055f7e7a90d56e3ba74393232785f8954e5f
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-07-23 22:24:31 +0530

    add the sign in and up buttons

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default function LandingPage() {
-  return <div>LandingPage(not protected)</div>;
+  return (
+    <div>
+      LandingPage(not protected)
+      <div>
+        <Link href={"/sign-in"}>
+          <Button>Sign In</Button>
+        </Link>
+        <Link href={"/sign-up"}>
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
